b1b6686a86c0187a92883496d564a87b47606a7b
Vez <chinveasna@gmail.com>
2024-12-11 14:59:00 -0700

    feat: wip switch to postgres with supabase

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,14 +1,12 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
+import { config } from "./src/config";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-export default {
+export default defineConfig({
   schema: "./src/schema/index.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
+  driver: "pglite",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: config.database.url,
   },
-} satisfies Config;
+});
--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,12 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
+import { config } from "./config";
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-const client = postgres(connectionString);
+const client = postgres(config.database.url);
 export const db = drizzle(client, { schema });
