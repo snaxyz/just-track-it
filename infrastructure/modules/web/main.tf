3558a5c4c01ea4d8281920cd5f148935d27b36c1
Vez <chinveasna@gmail.com>
2025-01-26 17:56:45 -0700

    allow ecs tasks to pull docker images

--- infrastructure/modules/web/main.tf
+++ infrastructure/modules/web/main.tf
@@ -1,9 +1,32 @@
+resource "aws_iam_role" "execution_role" {
+  name = "${var.app_name}-${var.environment}-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "execution_role_policy" {
+  role       = aws_iam_role.execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "web" {
   family                   = "${var.app_name}-${var.environment}"
   requires_compatibilities = ["FARGATE"]
   network_mode            = "awsvpc"
   cpu                     = var.cpu
   memory                  = var.memory
+  execution_role_arn      = aws_iam_role.execution_role.arn
 
   container_definitions = jsonencode([
     {
