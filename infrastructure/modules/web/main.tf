16695c1b9c71f30d3931cd70100d82659a668816
Vez <chinveasna@gmail.com>
2025-01-26 19:54:49 -0700

    fix alb

--- infrastructure/modules/web/main.tf
+++ infrastructure/modules/web/main.tf
@@ -102,19 +102,7 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web.arn
-  }
-}
-
-resource "aws_lb_target_group" "web" {
-  name        = "${var.app_name}-${var.environment}"
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    path = "/"
+    target_group_arn = aws_lb_target_group.blue.arn
   }
 }
 
