002d57801ea216edc24835f34fc4cf349cb2fcba
Vez <chinveasna@gmail.com>
2025-02-03 17:19:59 -0700

    fix outputs

--- infrastructure/modules/ecs/outputs.tf
+++ infrastructure/modules/ecs/outputs.tf
@@ -7,3 +7,8 @@ output "cluster_arn" {
   description = "The ARN of the ECS cluster"
   value       = aws_ecs_cluster.this.arn
 }
+
+output "cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,13 +1,11 @@
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster"
-  value       = module.ecs.cluster_id
+  value       = module.ecs.cluster_name
 }
 
 output "ecs_agent_service" {
-  description = "The names of the ECS services"
-  value = {
-    agent = module.agent.service_name
-  }
+  description = "The name of the ECS agent service"
+  value       = module.agent.service_name
 }
 
 output "agent_ecr_repository_url" {
